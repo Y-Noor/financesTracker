37fc0458ba9db96c800ddc9bbd7f4f787d6c07e7
Y-Noor <noor_yadallee@hotmail.com>
2023-10-31 00:36:03 +0400

    mvp.py v0.2

--- mvp.py
+++ mvp.py
@@ -2,21 +2,24 @@ class AddIncomeSource():
     def __init__(self, name, amount):
         self.grandparents = {name: {
             'amount':amount,
-            'parents':{},
+            'accounts':{},
             'children':{},
             'grandchildren':{}
             }}
+    
+    def addAccount(self, grandparent, name):
+        self.grandparents[grandparent]['accounts'] = {name:{}} 
 
     def check(self):
         print()
         for grandparent in self.grandparents:
             print('Grandparent: ', grandparent)
             print('Amount:', self.grandparents[grandparent]['amount'])
-            print('Parents', self.grandparents[grandparent]['parents'])
+            print('Parents', self.grandparents[grandparent]['accounts'])
             print('Children', self.grandparents[grandparent]['children'])
             print('Grandchildren', self.grandparents[grandparent]['grandchildren'])
 
-        print()
+        print() 
         print()
 
 
@@ -26,6 +29,7 @@ def main():
 1. Add recurring income source
 2. Add Account
 3. Add Category
+4. Check
 
 -1. exit
          ''') 
@@ -37,6 +41,9 @@ def main():
             name = input('input name: ')
             amount = float(input('Input amount: '))
             incomeSrc = AddIncomeSource(name, amount)       
+        case '2':
+            incomeSrc.addAccount(name, 'jajaja')
+        case '3':
             incomeSrc.check()
 
     return status
