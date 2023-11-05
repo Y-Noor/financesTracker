95e00e4003b6aee0f45dcd279b9404a755b26604
Y-Noor <noor_yadallee@hotmail.com>
2023-11-06 01:02:05 +0400

    mvp.py v0.7 - fixed ' and " convention

--- mvp.py
+++ mvp.py
@@ -7,52 +7,56 @@ class Schema():
         
     def addIncomeSource(self, sourceName, amount):
         self.grandparents[sourceName] = {
-                'amount': amount,
-                'accounts': [],
-                'remaining':amount
+                "amount": amount,
+                "accounts": [],
+                "remaining":amount
                 }
 
 
     def addAccount(self, grandparent, name, weight):
         identifier = weight[0]
-        remaining = self.grandparents[grandparent]['remaining']
-        amount = self.grandparents[grandparent]['amount']
-        
-        takeFromRemainingOrTakeFromSrc = input("Take from income source total or what is left over?(src/l): ")
+        remaining = self.grandparents[grandparent]["remaining"]
+        amount = self.grandparents[grandparent]["amount"]
+       
+        if name != "savings":
+            takeFromRemainingOrTakeFromSrc = input("Take from income source total or what is left over?(src/l): ")
+        else:
+            takeFromRemainingOrTakeFromSrc = "src"
         if takeFromRemainingOrTakeFromSrc == "src":
             if identifier == 'f':
                 val = float(weight[1:])
-                self.grandparents[grandparent]['accounts'].append({'name':name, 'weight':weight, 'srcOrLeft':takeFromRemainingOrTakeFromSrc, 'weight value': val, 'current':val})
-                self.grandparents[grandparent]['remaining'] = remaining - val
+                self.grandparents[grandparent]["accounts"].append({"name":name, "weight":weight, "srcOrLeft":takeFromRemainingOrTakeFromSrc, "weight value": val, "current":val})
+                self.grandparents[grandparent]["remaining"] = remaining - val
             elif identifier == '%':
                 val = amount * (float(weight[1:])/100)
-                self.grandparents[grandparent]['accounts'].append({'name':name, 'weight':weight, 'srcOrLeft':takeFromRemainingOrTakeFromSrc, 'weight value':val, 'current':val})
+                self.grandparents[grandparent]["accounts"].append({"name":name, "weight":weight, "srcOrLeft":takeFromRemainingOrTakeFromSrc, "weight value":val, "current":val})
                                                                                                                                          
          
 
-        elif takeFromRemainingOrTakeFromSrc == "l":
+        elif takeFromRemainingOrTakeFromSrc == 'l':
             if identifier == 'f':
                 val = float(weight[1:])
-                self.grandparents[grandparent]['accounts'].append({'name':name, 'weight':weight, 'srcOrLeft':takeFromRemainingOrTakeFromSrc, 'weight value': val, 'current':val})
-                self.grandparents[grandparent]['remaining'] = remaining - val
+                self.grandparents[grandparent]["accounts"].append({"name":name, "weight":weight, "srcOrLeft":takeFromRemainingOrTakeFromSrc, "weight value": val, "current":val})
+                self.grandparents[grandparent]["remaining"] = remaining - val
             elif identifier == '%':
                 val = remaining * (float(weight[1:])/100)
-                self.grandparents[grandparent]['accounts'].append({'name':name, 'weight':weight, 'srcOrLeft':takeFromRemainingOrTakeFromSrc, 'weight value':val, 'current':val})
+                self.grandparents[grandparent]["accounts"].append({"name":name, "weight":weight, "srcOrLeft":takeFromRemainingOrTakeFromSrc, "weight value":val, "current":val})
         
 
 
-        self.grandparents[grandparent]['remaining'] = remaining - val
+        self.grandparents[grandparent]["remaining"] = remaining - val
                                                                                                                                     
 
     def check(self):
         print()
         for grandparent in self.grandparents:
             print("==================================================")
-            print('Grandparent: ', grandparent)
-            print('Amount:', self.grandparents[grandparent]['amount'])
-            print('Remaining:', self.grandparents[grandparent]['remaining'])
+            print("Grandparent: ", grandparent)
+            print("Amount:", self.grandparents[grandparent]["amount"])
+            print("Remaining:", self.grandparents[grandparent]["remaining"])
+            
 
-            for parent in self.grandparents[grandparent]['accounts']:
+            for parent in self.grandparents[grandparent]["accounts"]:
                 print(parent)
             print()
             print("==================================================")
@@ -65,16 +69,16 @@ class Schema():
         return [key for key in  self.grandparents]
 
 def save():
-    with  open('finances', 'wb') as f:    
+    with  open("finances", "wb") as f:    
         pickle.dump(userData, f)
 
 def loadUserdata():
     global userData
     try:
-        with open('finances', 'rb') as f:
+        with open("finances", "rb") as f:
             userData =  pickle.load(f)
     except FileNotFoundError:
-        print('no current save file')
+        print("no current save file")
         userData = Schema()
 
 
@@ -83,41 +87,41 @@ def main():
     global userData
     status = True
 
-    print('''
+    print("""
 1. Add recurring income source
 2. Add Account
 3. Add Category
 4. Check
 
 -1. exit
-         ''') 
-    choice = input('What do you want to do? ')
+         """) 
+    choice = input("What do you want to do? ")
     match choice:
         case '-1':
             status = False
         case '1':
-            incomeSrcName = input('input name: ')
-            amount = float(input('Input amount: '))
+            incomeSrcName = input("input name: ")
+            amount = float(input("Input amount: "))
             userData.addIncomeSource(incomeSrcName, amount)
-            savingsBool = input('Do you want a savings account with this income source? (y/n): ')
+            savingsBool = input("Do you want a savings account with this income source? (y/n): ")
             if savingsBool == 'y':
-                flatOrPercentage = input('Flat or percentage for savings account? (f/p)')
+                flatOrPercentage = input("Flat or percentage for savings account? (f/p)")
                 if flatOrPercentage == 'f':
-                    flatAmt = 'f' + input('Input flat amount: ')
-                    userData.addAccount(incomeSrcName, 'Savings', flatAmt)
+                    flatAmt = 'f' + input("Input flat amount: ")
+                    userData.addAccount(incomeSrcName, "Savings", flatAmt)
                 elif flatOrPercentage == 'p':
-                    percentage = '%' + input('Input percentage of amount to add to savings: ')
-                    userData.addAccount(incomeSrcName, 'Savings', percentage)
+                    percentage = '%' + input("Input percentage of amount to add to savings: ")
+                    userData.addAccount(incomeSrcName, "Savings", percentage)
 
         case '2':
             inputSources = userData.getSources()
-            incomeSrcName = input(f'Input name of input source{inputSources}: ')
-            accountName = input('Input name of new account: ')
-            flatOrPercentage = input('Flat or percentagefor account? (f/p)')
+            incomeSrcName = input(f"Input name of input source{inputSources}: ")
+            accountName = input("Input name of new account: ")
+            flatOrPercentage = input("Flat or percentagefor account? (f/p)")
             if flatOrPercentage == 'f':
-                weight = 'f' + input('Input flat amount')
+                weight = 'f' + input("Input flat amount")
             elif flatOrPercentage == 'p':
-                weight = '%' + input('Input percentage of amount to go to this account: ')
+                weight = '%' + input("Input percentage of amount to go to this account: ")
             userData.addAccount(incomeSrcName, accountName, weight)
         case '3':
             pass
@@ -129,7 +133,7 @@ def main():
 
 
 #start    
-if __name__ == '__main__':
+if __name__ == "__main__":
     status = True
     if userData == None:
         loadUserdata()
